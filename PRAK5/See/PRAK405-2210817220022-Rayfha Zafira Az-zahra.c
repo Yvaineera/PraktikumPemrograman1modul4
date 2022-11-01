2a972f3eadfc22ac9456529193d2cdbb4f92c45f
Yvaineera <114866127+Yvaineera@users.noreply.github.com>
2022-11-01 13:52:42 +0800

    Create PRAK405-2210817220022-Rayfha Zafira Az-zahra.c

--- PRAK5/See/PRAK405-2210817220022-Rayfha Zafira Az-zahra.c
+++ PRAK5/See/PRAK405-2210817220022-Rayfha Zafira Az-zahra.c
@@ -6,7 +6,7 @@ void main(){
     scanf("%d %d", &a, &b);
     for(i = 1; i <= a; i++){
         for(j = i; j > 1; j--){printf("(%d * %d) +", j, b);}
-        for(o = 1, hsl = o * b; o < i; o++, hsl += (o*p));{
+        for(o = 1, hsl = o * b; o < i; o++, hsl += (o*b));{
         printf("(%d * %d) = %d\n", j, b , hsl);}
     for(p = 1, q = 1, t = 0; p <= a; q += p + 1, p++){
         t += q * b;}}
